d0c8ab88066f082bb9c8f3b836e17a9120aeed53
TomRivaud <temtonik@gmail.com>
2018-11-09 23:58:18 +0100

    Script move platform

--- GameJamFlat226/Assets/Scripts/MovePlatform.cs
+++ GameJamFlat226/Assets/Scripts/MovePlatform.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class MovePlatform : MonoBehaviour {
 
+    public float moveSpeedX;
+    public float moveSpeedY;
+    public bool moveRight = true;
+    public float secondToWait;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,44 @@ public class MovePlatform : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+		if (moveRight == true)
+        {
+            StartCoroutine(MovementSwitchLeft());
+            transform.position = new Vector2(transform.position.x + moveSpeedX * Time.deltaTime, transform.position.y + moveSpeedY * Time.deltaTime);
+        }
+        else
+        {
+            StartCoroutine(MovementSwitchRight());
+            transform.position = new Vector2(transform.position.x - moveSpeedX * Time.deltaTime, transform.position.y - moveSpeedY * Time.deltaTime);
+        }
+    }
+
+    IEnumerator MovementSwitchLeft()
+    {
+        yield return new WaitForSeconds(secondToWait);
+        moveRight = false;
+    }
+
+    IEnumerator MovementSwitchRight()
+    {
+        yield return new WaitForSeconds(secondToWait);
+        moveRight = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.collider.transform.SetParent(transform);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.collider.transform.SetParent(null);
+        }
+    }
 }
