3f9d924da690ee906eb3ebfe0322176b8afec83e
TomRivaud <temtonik@gmail.com>
2018-11-09 23:11:22 +0100

    RotatePlatform

---
+++ GameJamFlat226/Assets/RotatePlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlatform : MonoBehaviour {
+
+    public float moveSpeedZ;
+    public float moveSpeedY;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(Time.deltaTime * 0, Time.deltaTime * moveSpeedY, Time.deltaTime * moveSpeedZ));
+	}
+}
