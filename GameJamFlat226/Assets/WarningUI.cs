97471d2c53ccc1b360d41da90ea7d3be292d67ea
TomRivaud <temtonik@gmail.com>
2018-11-10 18:44:22 +0100

    Warning

---
+++ GameJamFlat226/Assets/WarningUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningUI : MonoBehaviour {
+
+    public PlayerMovement my_PlayerMovement;
+
+    public GameObject jumpWarning;
+    public GameObject moveWarning;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (my_PlayerMovement.IsInputJumpEnabled)
+        {
+            jumpWarning.SetActive(false);
+        }
+        if (!my_PlayerMovement.IsInputJumpEnabled)
+        {
+            jumpWarning.SetActive(true);
+        }
+
+
+        if (my_PlayerMovement.IsInputMoveEnabled)
+        {
+            moveWarning.SetActive(false);
+        }
+        if (!my_PlayerMovement.IsInputMoveEnabled)
+        {
+            moveWarning.SetActive(true);
+        }
+    }
+}
