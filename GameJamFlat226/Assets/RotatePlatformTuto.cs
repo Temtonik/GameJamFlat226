b3690405054851ce9b6bb006cc1f13c171b190ae
TomRivaud <temtonik@gmail.com>
2018-11-11 00:44:19 +0100

    Maj Tom

---
+++ GameJamFlat226/Assets/RotatePlatformTuto.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlatformTuto : MonoBehaviour {
+
+    public float moveSpeedZ;
+    public float secondToWait;
+
+    public bool switchRotate = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(switchRotate)
+        {
+            StartCoroutine(RotationSwitchLeft());
+            transform.Rotate(new Vector3(Time.deltaTime * 0, Time.deltaTime * 0, Time.deltaTime * moveSpeedZ));
+        }
+        if (!switchRotate)
+        {
+            StartCoroutine(RotationSwitchRight());
+            transform.Rotate(new Vector3(Time.deltaTime * 0, Time.deltaTime * 0, Time.deltaTime * -moveSpeedZ));
+        }
+    }
+
+    IEnumerator RotationSwitchLeft()
+    {
+        yield return new WaitForSeconds(secondToWait);
+        switchRotate = false;
+    }
+
+    IEnumerator RotationSwitchRight()
+    {
+        yield return new WaitForSeconds(secondToWait);
+        switchRotate = true;
+    }
+}
