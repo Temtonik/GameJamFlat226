493a1be5f45b19658c4c12c2fa5fc2178258d680
TomRivaud <temtonik@gmail.com>
2018-11-11 14:40:49 +0100

    Finalscene

---
+++ GameJamFlat226/Assets/TimerSwitchScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimerSwitchScene : MonoBehaviour {
+
+    public float DelayTime;
+    public string NextLevel;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SwitchScene());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SwitchScene()
+    {
+        yield return new WaitForSeconds(DelayTime);
+        SceneManager.LoadScene(NextLevel);
+    }
+}
