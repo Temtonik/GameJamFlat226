7217a103e68a00a7f1140f6ab9e88cdd40692a24
TomRivaud <temtonik@gmail.com>
2018-11-14 17:00:53 +0100

    Epilepsy

---
+++ GameJamFlat226/Assets/WarningSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WarningSwitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Delay());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene("Main");
+    }
+}
